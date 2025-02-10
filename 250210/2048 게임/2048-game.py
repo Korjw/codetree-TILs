92fa153461547fd91e4e7d30c8918beb2c22d6ab
김진우 <93375024+Korjw@users.noreply.github.com>
2025-02-10 22:01:20 +0900

    Title: 2048 게임, Time: 329ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 250210/2048 게임/2048-game.py
+++ 250210/2048 게임/2048-game.py
@@ -41,13 +41,82 @@ def left2(i,j):
         grid[i][l] = grid[i][l+1]
     grid[i][n-1] = 0
 
-def sum():
+def up_sum():
     #합치기
-    return
-    
-for i in range(n):
     for j in range(n):
-        print(grid[i][j], end = ' ')
-    print()
+        for i in range(n-1):
+            if grid[i][j] == grid[i+1][j]:
+                grid[i][j] *= 2
+                grid[i+1][j] = 0
+    return
 
+def down_sum():
+    #합치기
+    for j in range(n):
+        for i in range(n-1,0,-1):
+            if grid[i][j] == grid[i-1][j]:
+                grid[i][j] *= 2
+                grid[i-1][j] = 0
+    return
+
+def left_sum():
+    #합치기
+    for i in range(n):
+        for j in range(n-1):
+            if grid[i][j] == grid[i][j+1]:
+                grid[i][j] *= 2
+                grid[i][j+1] = 0
+    return
+
+def right_sum():
+    #합치기
+    for i in range(n):
+        for j in range(n-1,0,-1):
+            if grid[i][j] == grid[i][j-1]:
+                grid[i][j] *= 2
+                grid[i][j-1] = 0
+    return
+
+temp_try = []
+try_list = []
+
+def make_try(cnt):
+    if cnt == 5:
+        try_list.append(temp_try[:])
+        return
+    
+    for i in range(4):
+        temp_try.append(i)
+        make_try(cnt+1)
+        temp_try.pop()
+
+make_try(0)
+
+result = -1
+for tries_list in try_list:
+    for tries in tries_list:
+        max_count = -1
+        if tries == 0:
+            up()
+            up_sum()
+            up()
+        elif tries == 1:
+            down()
+            down_sum()
+            down()
+        elif tries == 2:
+            left()
+            left_sum()
+            left()
+        else:
+            right()
+            right_sum()
+            right()
+        for i in range(n):
+            for j in range(n):
+                if max_count < grid[i][j]:
+                    max_count = grid[i][j]
+        result = max(result, max_count)
+
+print(result)
 # 함 이동 -> 합치기 -> 다시 함 이동
