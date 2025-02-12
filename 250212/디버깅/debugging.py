5ec433766625c788c2fc6185c520c79b2d5a6661
김진우 <93375024+Korjw@users.noreply.github.com>
2025-02-12 12:27:33 +0900

    Title: 디버깅, Time: 79ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250212/디버깅/debugging.py
+++ 250212/디버깅/debugging.py
@@ -21,6 +21,7 @@ temp_pos = []
 temp_line_pos = []
 poss_pos = []
 poss_line_pos = []
+visited = [[False for _ in range(n)] for __ in range(h+1)]
 
 def make_pos(cnt, max_num):
     global temp_pos, temp_line_pos
@@ -31,25 +32,35 @@ def make_pos(cnt, max_num):
     
     for i in range(1, h+1):
         for j in range(n):
-            if grid[i][j] == 0 and line_grid[i][j] == 0 and [i,j] not in temp_pos and [i,j] not in temp_line_pos and [i,j+1] not in temp_line_pos :
+            if not visited[i][j] and grid[i][j] == 0 and line_grid[i][j] == 0 and [i,j] not in temp_pos and [i,j] not in temp_line_pos and [i,j+1] not in temp_line_pos :
                 temp_pos.append([i,j])
                 temp_line_pos.append([i,j])
                 temp_line_pos.append([i,j+1])
+                visited[i][j] = True
                 make_pos(cnt+1, max_num)
                 temp_pos.pop()
                 temp_line_pos.pop()
                 temp_line_pos.pop()
+                visited[i][j] = False
 
+make_pos(0,1)
+temp_pos = []
+temp_line_pos = []
 make_pos(0,2)
 temp_pos = []
 temp_line_pos = []
-# make_pos(0,2)
-# temp_pos = []
-# temp_line_pos = []
-# make_pos(0,3)
+make_pos(0,3)
 
 print(poss_pos)
 
+def simul():
+    try_grid = [[0 for _ in range(n)] for __ in range(h+1)]
+    try_line_grid = [[0 for _ in range(n)] for __ in range(h+1)]
+    # 1. 시뮬레이션 하기 try grid에 그리기
+
+def move():
+    # 2. 이동해보고
+    가능하면 끝
 
 for i in range(h+1):
     for j in range(n):
