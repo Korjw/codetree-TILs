2d86e5225af7f8f5839a97c05185d57c78910c48
김진우 <93375024+Korjw@users.noreply.github.com>
2025-02-13 12:43:54 +0900

    Title: 드래곤 커브, Time: 78ms, Memory: 19MB, Status: Runtime Error - CodeTree

--- 250213/드래곤 커브/dragon-curve.py
+++ 250213/드래곤 커브/dragon-curve.py
@@ -8,6 +8,16 @@ grid = [[[] for _ in range(101)] for __ in range(101)] # 어디랑 이어진지 
 dragon = [] #n 차 드래곤
 first, last = [0,0], [0,0] # 시작점, 끝점
 rotate_grid =[[0 for _ in range(11)] for __ in range(11)]
+dragon_list =[]
+
+def out_of_range(x,y):
+    return x < 0 or y < 0 or x > 10 or y > 10
+
+def check_grid(x, y):
+    global grid, rotate_grid
+    for dir_x, dir_y in zip(dx,dy):
+        if not out_of_range(x+dir_x, y+dir_y) and rotate_grid[x+dir_x][y+dir_y]:
+            grid[x][y].append([x+dir_x, y+dir_y])
 
 def roate(): # x, y는 끝점
     global last, rotate_grid
@@ -28,14 +38,19 @@ def roate(): # x, y는 끝점
     for i in range(11):
         for j in range(11):
             if rotate_grid[i][j]:
-                grid[i-diff_x][j-diff_y] = 1
+                rotate_grid[i][j] = 0
                 rotate_grid[i-diff_x][j-diff_y] = 1
                 if [i-diff_x, j-diff_y] != last:
                     print(last)
                     dragon.append([i-diff_x, j-diff_y])
-                rotate_grid[i][j] = 0
     print()
     
+    for i in range(11):
+        for j in range(11):
+            if rotate_grid[i][j]:
+                check_grid(i, j)
+    print()
+
     max_last_x, max_last_y, max_last_dist = -1, -1, -1
     for i in range(11):
         for j in range(11):
@@ -68,11 +83,15 @@ for _ in range(1):
     first[0], first[1] = x, y
     last[0], last[1] = x + dx[d], y + dy[d]
     print(first,last)
-    grid[first[0]][first[1]].append([last[0]][last[1]])
+    grid[first[0]][first[1]].append(last[:])
+    grid[last[0]][last[1]].append(first[:])
     dragon.append(first)
     dragon.append(last[:])
     draw_dragon(x,y,d,g)
+    # dragon_list 추가 grid에서 이어졌는지 체크 ex. [i+1, j], [i, j+1]
+    # i,j i+1,j+1 까지 4개 조가
 
+print(dragon)
 
 for i in range(11):
     for j in range(11):
