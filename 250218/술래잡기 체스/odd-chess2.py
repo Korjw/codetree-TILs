baabe43180433682d2afc685226f04ccf36a1fef
김진우 <93375024+Korjw@users.noreply.github.com>
2025-02-18 20:18:52 +0900

    Title: 술래잡기 체스, Time: 93ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250218/술래잡기 체스/odd-chess2.py
@@ -0,0 +1,57 @@
+grid =[[[] for _ in range(4)] for __ in range(4)]
+horse_list = []
+dx, dy = [-1,-1,0,1,1,1,0,-1], [0,-1,-1,-1,0,1,1,1]
+
+for i in range(4):
+    p1,d1,p2,d2,p3,d3,p4,d4 = list(map(int,input().split()))
+    grid[i][0] = [p1,d1-1]
+    grid[i][1] = [p2,d2-1]
+    grid[i][2] = [p3,d3-1]
+    grid[i][3] = [p4,d4-1]
+    horse_list.append([i,0,p1,d1-1])
+    horse_list.append([i,1,p2,d2-1])
+    horse_list.append([i,2,p3,d3-1])
+    horse_list.append([i,3,p4,d4-1])
+
+sulrae = [[0,0,grid[0][0][1]]] # x 위치, y 위치, 방향
+horse_list.remove([0,0,grid[0][0][0],grid[0][0][1]])
+grid[0][0] = [-1,-1]
+score = [[0,0,grid[0][0][0],grid[0][0][1]]]
+
+for i in range(4):
+    for j in range(4):
+        print(grid[i][j], end = ' ')
+    print()
+
+def out_of_range(x,y):
+    return x < 0 or y < 0 or x > n-1 or y > n-1
+
+def move(end):
+    if end:
+        return
+    while end != True:
+        print(end)
+        count = 0
+        for i in range(4):
+            curr_x, curr_y, direction = sulrae[-1][0], sulrae[-1][1], sulrae[-1][2]
+            if not out_of_range(curr_x+dx[direction]*i, curr_y+dy[direction]*i):
+                sulrae.append([cu])
+                move(end)
+            end = 함수
+
+            if end != True:
+                move(end)
+                count+=1
+
+            score.pop()
+            sulrae.pop()
+            # grid 정상화
+            # horse_list 정상화
+        if count == 0:
+            end = True
+        
+move(False)
+
+print(sulrae)
+horse_list.sort(key = lambda x : -x[2])
+print(horse_list)
