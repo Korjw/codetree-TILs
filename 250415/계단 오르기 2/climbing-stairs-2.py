4b2a47736b48c9843099c9ae6f12c62c9b56b722
김진우 <93375024+Korjw@users.noreply.github.com>
2025-04-15 23:10:59 +0900

    Title: 계단 오르기 2, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250415/계단 오르기 2/climbing-stairs-2.py
+++ 250415/계단 오르기 2/climbing-stairs-2.py
@@ -2,16 +2,17 @@ n = int(input())
 coin = [0] + list(map(int, input().split()))
 
 dp = [[0 for _ in range(4)] for __ in range(n+1)] # i번째 서있음, j번 1계단 갔음
-for j in range(4):
-    dp[0][j] = 0
 dp[1][1] = coin[1]
 if n > 1:
     dp[2][0] = coin[2]
     dp[2][2] = coin[1] + coin[2]
 
 for i in range(3,n+1):
-    for j in range(1,4):
-        dp[i][j] = max(dp[i-1][j-1] + coin[i], dp[i-2][j] + coin[i], dp[i][j])
+    for j in range(4):
+        if j and dp[i-1][j-1] != 0:
+            dp[i][j] = max(dp[i-1][j-1] + coin[i], dp[i][j])
+        if dp[i-2][j] != 0:
+            dp[i][j] = max(dp[i-2][j] + coin[i], dp[i][j])
 
 # for i in range(n+1):
 #     for j in range(4):
