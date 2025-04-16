e1df71cda60731a86beaaa437a1e0e98c74a5ed6
김진우 <93375024+Korjw@users.noreply.github.com>
2025-04-17 03:56:35 +0900

    Title: 두 사람, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250417/두 사람/two-person.py
+++ 250417/두 사람/two-person.py
@@ -1,6 +1,6 @@
 a1,a2 = map(str,input().split())
 b1,b2 = map(str,input().split())
-if (int(a1) >= 1 and a2 == 'M') or (int(b1) >= 1 and b2 == 'M'):
+if (int(a1) >= 19 and a2 == 'M') or (int(b1) >= 19 and b2 == 'M'):
     print(1)
 else:
     print(0)
