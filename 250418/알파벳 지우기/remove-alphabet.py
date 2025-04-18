693f1dc7fe2177cf2de5388bfd76447b939392e8
김진우 <93375024+Korjw@users.noreply.github.com>
2025-04-18 20:27:45 +0900

    Title: 알파벳 지우기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250418/알파벳 지우기/remove-alphabet.py
+++ 250418/알파벳 지우기/remove-alphabet.py
@@ -3,10 +3,10 @@ s2 = str(input())
 
 s3, s4 = "", ""
 for i in s1:
-    if i >= '0' and i < '9':
+    if i >= '0' and i <= '9':
         s3 += i
 for i in s2:
-    if i >= '0' and i < '9':
+    if i >= '0' and i <= '9':
         s4 += i
 
 print(int(s3)+int(s4))
