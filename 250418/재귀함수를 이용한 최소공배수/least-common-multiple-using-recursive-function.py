629793138c452614bf320efdaf2673182e86c86f
김진우 <93375024+Korjw@users.noreply.github.com>
2025-04-18 21:21:47 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 125ms, Memory: 22MB, Status: Runtime Error - Codetree

---
+++ 250418/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+def func(x):
+    cnt = 0
+    for i in arr:
+        if x % i == 0:
+            cnt += 1
+    if cnt == len(arr):
+        print(x)
+        return
+    func(x+1)
+func(1)
+# Please write your code here.
