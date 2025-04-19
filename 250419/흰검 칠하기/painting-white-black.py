307d6de3d135ee01a7d758240fe94519cab80b7e
김진우 <93375024+Korjw@users.noreply.github.com>
2025-04-19 20:04:45 +0900

    Title: 흰검 칠하기, Time: 71ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250419/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,28 @@
+n = int(input())
+temp = [0 for _ in range(206)]
+curr = 103
+b = [0 for _ in range(206)]
+w = [0 for _ in range(206)]
+
+for _ in range(n):
+    x, c = map(str,input().split())
+    x = int(x)
+    #print(curr)
+    if c == 'L':
+        for i in range(curr-x+1,curr+1):
+            if temp[i] != 3:
+                temp[i] = 1
+                w[i] += 1
+            if b[i] >= 2 and w[i] >= 2:
+                temp[i] = 3
+        curr = curr-x+1
+    else:
+        for i in range(curr,curr+x):
+            if temp[i] != 3:
+                temp[i] = 2
+                b[i] += 1
+            if b[i] >= 2 and w[i] >= 2:
+                temp[i] = 3
+        curr = curr+x-1
+
+print(temp.count(1),temp.count(2),temp.count(3))
