cd2efceef128acdaff0a81f1ef9f2ab0d2073729
김진우 <93375024+Korjw@users.noreply.github.com>
2025-04-19 21:33:52 +0900

    Title: 스승의 은혜 3, Time: 310ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 250419/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250419/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -5,15 +5,15 @@ result = 0
 for _ in range(n):
     p,s = map(int,input().split())
     ps.append([p,s])
-ps.sort()
+
 for i in range(n):
+    temp = ps[:]
+    temp[i][0] //= 2
+    temp.sort(key = lambda x : (x[0]+x[1]))
     cnt = 0
     count = 0
     for j in range(n):
-        if j == i:
-            cnt += (ps[i][0] // 2 + ps[i][1])
-        else:
-            cnt += (ps[j][0] + ps[j][1])
+        cnt += (temp[j][0] + temp[j][1])
         if cnt <= b:
             count += 1
             result = max(result,count)
